e5dca1b0a33407995f8066912f8086b530af8e2b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-19 12:28:37 -0400

    understanding closures part 2

--- app.js
+++ app.js
@@ -697,11 +697,53 @@
 // UNDERSTANDING CLOSURES
 // =================================================================================
 
-function greet(whattosay) {
-  return function (name) {
-    console.log(`${whattosay} ${name}`);
-  };
+// function greet(whattosay) {
+//   return function (name) {
+//     console.log(`${whattosay} ${name}`);
+//   };
+// }
+
+// var sayHi = greet('Hi');
+// sayHi('Davin');
+
+// =================================================================================
+// UNDERSTANDING CLOSURES PART 2
+// =================================================================================
+
+function buildFunctions() {
+  var arr = [];
+
+  for (var i = 0; i < 3; i++) {
+    arr.push(function () {
+      console.log(i);
+    });
+  }
+
+  return arr;
 }
 
-var sayHi = greet('Hi');
-sayHi('Davin');
+var fs = buildFunctions();
+fs[0]();
+fs[1]();
+fs[2]();
+
+function buildFunctions2() {
+  var arr = [];
+
+  for (var i = 0; i < 3; i++) {
+    arr.push(
+      (function (j) {
+        return function () {
+          console.log(j);
+        };
+      })(i)
+    );
+  }
+
+  return arr;
+}
+
+var fs2 = buildFunctions2();
+fs2[0]();
+fs2[1]();
+fs2[2]();
