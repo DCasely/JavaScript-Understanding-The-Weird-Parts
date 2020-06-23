f605cb463540d2fdf7579c9ec24c350d5d0317c2
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-23 16:11:39 -0400

    ES6 AND CLASSES

--- app.js
+++ app.js
@@ -1140,30 +1140,63 @@
 // OBJECT.CREATE AND PURE PROTOTYPAL INHERITANCE
 // =================================================================================
 
-if (!Object.create) {
-  Object.create = function (o) {
-    if (arguments.length > 1) {
-      throw new Error(
-        'Object.create implementation only accepts the first parameter'
-      );
-    }
-    function F() {}
-    F.prototype = o;
-    return new F();
-  };
+// // POLYFILL: CODE THAT ADDS A FEATURE WHICH THE ENGINE MAY LACK.
+
+// // POLYFILL
+// if (!Object.create) {
+//   Object.create = function (o) {
+//     if (arguments.length > 1) {
+//       throw new Error(
+//         'Object.create implementation only accepts the first parameter'
+//       );
+//     }
+//     function F() {}
+//     F.prototype = o;
+//     return new F();
+//   };
+// }
+
+// var person = {
+//   firstname: 'Default',
+//   lastname: 'Default',
+//   greet: function () {
+//     return `Hi ${this.firstname} ${this.lastname}`;
+//   },
+// };
+
+// var john = Object.create(person);
+// john.firstname = 'John';
+// john.lastname = 'Wayne';
+// console.log(john.greet());
+
+// =================================================================================
+// ES6 AND CLASSES
+// =================================================================================
+
+//SYNTACTIC SUGAR: A DIFFERENT WAY TO TYPE SOMETHING THAT DOESN'T CHANGE HOW IT WORKS UNDER THE HOOD
+class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  greet() {
+    return `Hi ${this.firstname}`;
+  }
 }
 
-var person = {
-  firstname: 'Default',
-  lastname: 'Default',
-  greet: function () {
-    return `Hi ${this.firstname} ${this.lastname}`;
-  },
-};
+class InformalPerson extends Person {
+  constructor(firstname, lastname) {
+    super(firstname, lastname);
+  }
 
-var john = Object.create(person);
-john.firstname = 'John';
-john.lastname = 'Wayne';
-console.log(john.greet());
+  greet() {
+    return `Yo ${this.firstname}`;
+  }
+}
 
-// POLYFILL: CODE THAT ADDS A FEATURE WHICH THE ENGINE MAY LACK.
+const davin = new Person('Davin', 'Casely');
+console.log(davin.greet());
+
+const informal = new InformalPerson('inform', 'thisguy');
+console.log(informal.greet());
