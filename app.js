26c72336c4144a18772888078727933d3c48483f
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-23 11:16:22 -0400

    Reflection and Extend

--- app.js
+++ app.js
@@ -969,6 +969,45 @@
 // EVERYTHING IS AN OBJECT (OR A PRIMITIVE)
 // =================================================================================
 
-var a = {};
-var b = function () {};
-var c = [];
+// var a = {};
+// var b = function () {};
+// var c = [];
+
+// =================================================================================
+// REFLECTION AND EXTEND
+// =================================================================================
+
+// REFLECTION: AN OBJECT CAN LOOK AT ITSELF, LISTING AND CHANGING ITS PROPERTIES AND METHODS.
+
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function () {
+    return `${this.firstname} ${this.lastname}`;
+  },
+};
+
+var john = {
+  firstname: 'John',
+  lastname: 'Doe',
+};
+
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+
+for (const key in john) {
+  if (john.hasOwnProperty(key)) console.log(`${key}: ${john[key]}`);
+}
+
+var jane = {
+  address: '111 Main St.',
+  getFormalFullName: function () {
+    return `${this.lastname}, ${this.firstname}`;
+  },
+};
+
+var jim = {
+  getFirstName: function () {
+    return firstname;
+  },
+};
