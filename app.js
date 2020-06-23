630f78086f5729b2104584fb8d71917629d4ff5b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-22 20:06:36 -0400

    Functional Programming Part 2

--- app.js
+++ app.js
@@ -865,49 +865,62 @@
 // FUNCTIONAL PROGRAMMING
 // =================================================================================
 
-function mapForEach(arr, fn) {
-  var newArr = [];
+// function mapForEach(arr, fn) {
+//   var newArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(fn(arr[i]));
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.push(fn(arr[i]));
+//   }
 
-  return newArr;
-}
+//   return newArr;
+// }
+
+// var arr1 = [1, 2, 3];
+// console.log(arr1);
+
+// // var arr2 = [];
+// // for (var i = 0; i < arr1.length; i++) {
+// //   arr2.push(arr1[i] * 2);
+// // }
+
+// var arr2 = mapForEach(arr1, function (item) {
+//   return item * 2;
+// });
+
+// console.log(arr2);
+
+// var arr3 = mapForEach(arr1, function (item) {
+//   return item > 2;
+// });
+
+// console.log(arr3);
+
+// var checkPastLimit = function (limiter, item) {
+//   return item > limiter;
+// };
+
+// var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+
+// console.log(arr4);
+
+// var checkPastLimitSimplified = function (limiter) {
+//   return function (limiter, item) {
+//     return item > limiter;
+//   }.bind(this, limiter);
+// };
+
+// var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
+// console.log(arr5);
+
+// =================================================================================
+// FUNCTIONAL PROGRAMMING PART 2
+// =================================================================================
 
 var arr1 = [1, 2, 3];
 console.log(arr1);
 
-// var arr2 = [];
-// for (var i = 0; i < arr1.length; i++) {
-//   arr2.push(arr1[i] * 2);
-// }
+var arr6 = arr1.map((item) => item * 3);
+console.log(arr6);
 
-var arr2 = mapForEach(arr1, function (item) {
-  return item * 2;
-});
-
-console.log(arr2);
-
-var arr3 = mapForEach(arr1, function (item) {
-  return item > 2;
-});
-
-console.log(arr3);
-
-var checkPastLimit = function (limiter, item) {
-  return item > limiter;
-};
-
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
-
-console.log(arr4);
-
-var checkPastLimitSimplified = function (limiter) {
-  return function (limiter, item) {
-    return item > limiter;
-  }.bind(this, limiter);
-};
-
-var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
-console.log(arr5);
+var arr7 = [2, 3, 4, 5, 6, 7].filter((x) => x % 2 === 0);
+console.log(arr7);
