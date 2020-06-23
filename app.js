386c17b1ff72b7feba4fee4c31ab3f97a0e22ebb
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-23 16:01:52 -0400

    OBJECT.CREATE AND PURE PROTOTYPAL INHERITANCE

--- app.js
+++ app.js
@@ -1124,14 +1124,46 @@
 // DANGEROUS ASIDE: ARRAYS AND FOR...IN
 // =================================================================================
 
-Array.prototype.myCustomFeature = 'cool!';
+// Array.prototype.myCustomFeature = 'cool!';
 
-var arr = ['John', 'Jane', 'Jim'];
+// var arr = ['John', 'Jane', 'Jim'];
 
-for (const key in arr) {
-  console.log(`${key}: ${arr[key]}`);
+// for (const key in arr) {
+//   console.log(`${key}: ${arr[key]}`);
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${i}: ${arr[i]}`);
+// }
+
+// =================================================================================
+// OBJECT.CREATE AND PURE PROTOTYPAL INHERITANCE
+// =================================================================================
+
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error(
+        'Object.create implementation only accepts the first parameter'
+      );
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
 }
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(`${i}: ${arr[i]}`);
-}
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  greet: function () {
+    return `Hi ${this.firstname} ${this.lastname}`;
+  },
+};
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Wayne';
+console.log(john.greet());
+
+// POLYFILL: CODE THAT ADDS A FEATURE WHICH THE ENGINE MAY LACK.
