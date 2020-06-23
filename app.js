018e4b73fbc5fb7ca6a2d67524e00b50ea449044
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-23 15:12:47 -0400

    Dangerous Aside: 'new' and functions

--- app.js
+++ app.js
@@ -1037,6 +1037,30 @@
 // FUNCTION CONSTRUCTORS, and '.prototype'
 // =================================================================================
 
+// function Person(firstname, lastname) {
+//   this.firstname = firstname;
+//   this.lastname = lastname;
+// }
+
+// Person.prototype.getFullName = function () {
+//   return `${this.firstname} ${this.lastname}`;
+// };
+
+// var john = new Person('John', 'Stockton');
+// console.log(john);
+
+// var jane = new Person('Jane', 'Wallace');
+// console.log(jane);
+// Person.prototype.getFormalFullName = function () {
+//   return `${this.lastname}, ${this.firstname}`;
+// };
+
+// console.log(john.getFormalFullName());
+
+// =================================================================================
+// DANGEROUS ASIDE: 'new' and functions
+// =================================================================================
+
 function Person(firstname, lastname) {
   this.firstname = firstname;
   this.lastname = lastname;
